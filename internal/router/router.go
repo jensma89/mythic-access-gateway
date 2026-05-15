d1510fa47d33d3b8492a7fbcd679678264554b07
Jens M. <jensmayer89@gmail.com>
2026-05-15 13:53:13 +0200

    Start creating the router file

--- internal/router/router.go
+++ internal/router/router.go
@@ -2,3 +2,14 @@
 // All routes
 
 package router
+
+import (
+	"database/sql"
+	"net/http"
+
+	"github.com/jensma89/mythic-access-gateway/internal/email"
+	"github.com/jensma89/mythic-access-gateway/internal/handlers"
+	"github.com/jensma89/mythic-access-gateway/internal/middleware"
+)
+
+// New builds and returns the main HTTP mux with all routes registered.
